2134428c54cd90a8d1b3bd300f2bbcb50f842672
Ben Avrahami <avrahami.ben@gmail.com>
2025-03-17 11:25:57 +0200

    Initial (#1)
    * wip
    
    * .
    
    * .
    
    * .
    
    ---------
    
    Co-authored-by: Ben Avrahami <wouldntyouliketoknow@weather.boy>

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# Yellowbox-MSSQL Changelog
+## 0.1.0
+- Initial release
---
+++ scripts/lint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# run various linters
+set -e
+echo "running black..."
+python -m ruff format . --check
+echo "running ruff..."
+python -m ruff check .
+echo "running mypy..."
+python3 -m mypy --show-error-codes yellowbox_mssql
